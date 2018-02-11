2d494e97a16f35d2e752d2a91c230c044e2aa974
Adam Plumer <caerus.karu@gmail.com>
2018-02-10 19:29:35 -0500

    feat(docs): add auto-generated docs with Dgeni
    * Add doc-generation tools from Material to generate docs from
      source files
    * Restructure directory structure to make it easier to organize
      new files and documentation
    * Bump deps for Dgeni and Angular

--- src/lib/api/ext/public-api.ts
+++ src/lib/api/ext/public-api.ts
@@ -5,4 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export * from './public-api';
+export * from './class/index';
+export * from './img-src/index';
+export * from './show-hide/index';
+export * from './style/index';
